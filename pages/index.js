90e41cc67bea021e8bad7d998d13168f1e272d58
Larissa Moro <54962550+LariMoro20@users.noreply.github.com>
2021-07-14 22:32:27 -0300

    [Aula 1] Components com React, Styled Components e NextJS

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,54 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSidebar(props) {
+  return (
+    <Box>
+      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }}></img>
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = 'LariMoro20';
+  const pessoasFav = ['juunegreiros', 'omariosouto', 'peas', 'rafaballerini', 'felipefialho', githubUser];
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar githubUser={githubUser} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1 className="title">Bem vindo(a)</h1>
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBoxWrapper >
+
+            <h2 className="smallTitle">Pessoas da Comunidade ({pessoasFav.length})</h2>
+            <ul>
+              {pessoasFav.map((person) => {
+                return (
+                  <li>
+                    <a href={`/users/${person}`} key={person}>
+                      <img src={`https://github.com/${person}.png`} />
+                      <span>{person}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          <Box style={{ gridArea: 'profileRelationsArea' }}>
+            comunidade
+          </Box>
+        </div>
+      </MainGrid>
+    </>
+  )
 }
