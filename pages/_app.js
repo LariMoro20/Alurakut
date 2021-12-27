36f38b3ce3744f176869c03fde2cb3db9df82022
Larissa Moro <54962550+LariMoro20@users.noreply.github.com>
2021-12-27 16:56:45 -0300

    [UPDATE] MetaTags

--- pages/_app.js
+++ pages/_app.js
@@ -44,10 +44,15 @@ export default function App({ Component, pageProps }) {
           <meta http-equiv="content-language" content="pt-br" />
           <meta http-equiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-          <meta name="description" content="Alurakut desenvolvido por Larissa Santos na Imersão React 3ª edição da Alura" />
+          <meta name="description" content="Atividade desenvolvida por Larissa Santos na Imersão React 3ª edição da Alura" />
           <meta name="author" content="Larissa Moro S. Santos" />
           <meta name="keywords" content="imersao, react, alura, curso, resultado" />
           <meta name="copyright" content="© 2022 Larissa Santos" />
+
+          <meta property="og:title" content="Alurakut - Larissa Santos" />
+          <meta property="og:description" content="Atividade desenvolvida por Larissa Santos na Imersão React 3ª edição da Alura" />
+          <meta property="og:url" content="https://alurakut-larimoro.vercel.app/" />
+          <meta property="og:image" content="/logo-favicon.ico" />
         </Head>
         <Component {...pageProps} />
       </ThemeProvider>
