cd55915f9ae6cf014fc8f5b67bd98cde3b6a3c1b
Larissa Moro <54962550+LariMoro20@users.noreply.github.com>
2021-12-27 16:09:18 -0300

    Update amigos.js

--- pages/amigos.js
+++ pages/amigos.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import nookies from 'nookies';
+import jwt from 'jsonwebtoken';
 import { AlurakutMenu, AlurakutProfileSidebarMenuDefault } from '../src/lib/AlurakutCommons'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
 import { ProfileFriendsRelationsWraper } from '../src/components/ProfileFriendsRelations'
+
+
+
 
 function FriendsBox(props) {
     return (
@@ -23,8 +31,7 @@ function FriendsBox(props) {
         </ProfileFriendsRelationsWraper >
     )
 }
-
-export default function Amigos(props) {
+export default function Home(props) {
     const githubUser = props.githubUser;
     const [followers, setFollowers] = React.useState([])
     React.useEffect(() => {
@@ -47,4 +54,3 @@ export default function Amigos(props) {
         </>
     )
 }
-
